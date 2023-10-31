c8c045c858272d5777f336f230ed8a02ed97b2f1
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2023-10-31 19:05:19 +0100

    Update - Added CreditHistory - Small fixes

---
+++ CasinoRoyale/CasinoRoyale/CreditHistory.cs
@@ -0,0 +1,14 @@
+﻿namespace CasinoRoyale
+{
+    class CreditHistory
+    {
+        public static void printCreditHistory(List<int> creditHistory)
+        {
+            Console.WriteLine("Your bet history\n");
+            foreach (int credits in creditHistory)
+            {
+                Console.WriteLine("Your new credit balance became: " + credits);
+            }
+        }
+    }
+}
